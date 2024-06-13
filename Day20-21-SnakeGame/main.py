ddc64111809724025d9e9f312c35ca47fc837cb6
uh_ibraheem <aby2004.im@gmail.com>
2024-06-13 18:03:36 -0400

    Snake Game complete

--- Day20-21-SnakeGame/main.py
+++ Day20-21-SnakeGame/main.py
@@ -1,17 +1,49 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from time import sleep
+from food import Food
+from snake import Snake
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
+screen.update()
 
-# Creating a snake body
-snake_body = []
-positions = [(0, 0), (-20, 0), (-40, 0)]
-for position in positions:
-    new_body = Turtle("square")
-    new_body.color("white")
-    new_body.setpos(position)
-    snake_body.append(new_body)
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    sleep(.08)
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # detects with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
